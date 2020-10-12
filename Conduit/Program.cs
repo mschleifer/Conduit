34d752e727b7f9827295a2da34fc6a036a127e99
Matt Schleifer <Matthew.Schleifer@wisc.edu>
2020-10-12 07:43:48 -0500

    clean up comments and unused refs

--- Conduit/Program.cs
+++ Conduit/Program.cs
@@ -1,11 +1,8 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Net.Http;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Conduit
@@ -30,23 +27,6 @@ namespace Conduit
             builder.Services.AddHttpClient<PublicClient>(client => client.BaseAddress = new Uri("https://conduit.productionready.io/"));
 
             //builder.Services.AddScoped<OrderState>();
-
-            // Add auth services
-            //builder.Services.AddOptions();
-            //builder.Services.AddAuthorizationCore();
-            //builder.Services.AddApiAuthorization<RemoteAuthenticationState>(options =>
-            //{
-            //    options.AuthenticationPaths.LogInPath = "login";
-            //    //options.AuthenticationPaths.LogInCallbackPath = "authentication/login-callback";
-            //    //options.AuthenticationPaths.LogInFailedPath = "authentication/login-failed";
-            //    //options.AuthenticationPaths.LogOutPath = "";
-            //    //options.AuthenticationPaths.LogOutCallbackPath = "authentication/logout-callback";
-            //    //options.AuthenticationPaths.LogOutFailedPath = "authentication/logout-failed";
-            //    options.AuthenticationPaths.LogOutSucceededPath = "";
-            //    options.AuthenticationPaths.ProfilePath = "settings";
-            //    options.AuthenticationPaths.RegisterPath = "register";
-            //});
-
 
             await builder.Build().RunAsync();
         }
