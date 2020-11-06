9e0e4bbcb85149c946fbea2d0a02d4ceb4f7a3a9
Matt Schleifer <Matthew.Schleifer@wisc.edu>
2020-11-05 23:02:44 -0600

    Article create function

--- Conduit/Models/Article.cs
+++ Conduit/Models/Article.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Conduit.Models
 {
     public class Article
     {
         public string Slug { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Body { get; set; }
         public List<string> TagList { get; set; }
         public DateTime CreatedAt { get; set; }
